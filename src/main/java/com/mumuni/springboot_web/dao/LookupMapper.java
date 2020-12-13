b168af4fd11058912842dd6d59abeb3ade6b2424
Hyun-jin Won <ohj8447@gmail.com>
2020-12-14 00:06:03 +0900

    중간 제출

---
+++ src/main/java/com/mumuni/springboot_web/dao/LookupMapper.java
@@ -0,0 +1,24 @@
+package com.mumuni.springboot_web.dao;
+
+import com.mumuni.springboot_web.rest_lookup.vo.CountOfPeriodVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+@Mapper
+public interface LookupMapper {
+//    public Integer getOrderCount(String order, LocalDate startDate, LocalDate endDate, String filter);
+//
+//    public List<CountOfPeriodVO> getOrderCountMonthly(String order, LocalDate startDate, LocalDate endDate, String filter);
+//    public List<CountOfPeriodVO> getOrderCountDaily(String order, LocalDate startDate, LocalDate endDate, String filter);
+
+    public List<CountOfPeriodVO> getConnectionCountMonthly(Map<String, String> map);
+//    public List<CountOfPeriodVO> getConnectionCountDaily(String order, LocalDate startDate, LocalDate endDate, String fileer);
+//
+//    public Double getOrderCountDailyAverage(String order, LocalDate startDate, LocalDate endDate, String filter);
+}
