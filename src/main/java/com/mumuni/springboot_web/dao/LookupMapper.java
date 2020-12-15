8eacc6f2e9f99119b39b7b6a9c3847b396745bf5
Hyun-jin Won <ohj8447@gmail.com>
2020-12-15 15:49:15 +0900

    cnf : add hikariCP, change jdbc maria to mysql
    1. hikariCP 적용 추가, application.properties 변경, DatabaseConfiuration config.class 추가
    2. CP와 Mybatis 연결을 위해 DatabaseConfiguration.class 에서 hikaridatasource를 datasource bean에 등록을 추가했습니다.

--- src/main/java/com/mumuni/springboot_web/dao/LookupMapper.java
+++ src/main/java/com/mumuni/springboot_web/dao/LookupMapper.java
@@ -4,7 +4,6 @@ import com.mumuni.springboot_web.rest_lookup.vo.CountOfPeriodVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -13,11 +12,11 @@ import java.util.Map;
 public interface LookupMapper {
     Long getOrderCountExceptWeekend(Map<String, String> map);
 
-    //List<CountOfPeriodVO> getOrderCountMonthly(Map<String, String> map);
-    //List<CountOfPeriodVO> getOrderCountDaily(Map<String, String> map);
-
     List<CountOfPeriodVO> getConnectionCountMonthly(Map<String, String> map);
     List<CountOfPeriodVO> getConnectionCountDaily(Map<String, String> map);
+
+    //List<CountOfPeriodVO> getOrderCountMonthly(Map<String, String> map);
+    //List<CountOfPeriodVO> getOrderCountDaily(Map<String, String> map);
 
     Long getOrderCount(Map<String, String> map);
 }
